8d49f4bb49323cb9907019ec6119b4d351653ac3
KseniaSafonova <kseniatsn@gmail.com>
2022-04-08 14:36:51 +0300

    add rendering

--- ecodictionary/src/Table/TableString.jsx
+++ ecodictionary/src/Table/TableString.jsx
@@ -1,19 +1,49 @@
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import styles from './Table.module.css'
 
 export default function TableString(props) {
+    const [pressed, setPressed] = useState(false);
+
+
+    const HandleClick = () => {
+        setPressed(!pressed);
+    }
+
+    const HandleCancel = () => {
+
+    }
     return (
 
         <tbody>
-            <tr>
-                <td>{props.id}</td>
-                <td>{props.english}</td>
-                <td>{props.transcription}</td>
-                <td>{props.russian}</td>
-                <td><Button variant="outline-warning">Edit</Button>
-                    <Button variant="outline-danger">Delete</Button>
-                </td>
-            </tr>
+            {
+                pressed ?
+                    <tr>
+                        <td>{props.id}</td>
+                        <td><input /></td>
+                        <td><input /></td>
+                        <td><input /></td>
+                        <td>
+                            <div className={styles.buttonBlock}>
+                                <Button variant="outline-success" onClick={HandleCancel} >Success</Button>
+                                <Button variant="outline-danger">Delete</Button>
+                            </div>
+                        </td>
+                    </tr>
+                    :
+                    <tr>
+                        <td>{props.id}</td>
+                        <td>{props.english}</td>
+                        <td>{props.transcription}</td>
+                        <td>{props.russian}</td>
+                        <td>
+                            <div className={styles.buttonBlock}>
+                                <Button variant="outline-warning" onClick={HandleClick} >Edit</Button>
+                                <Button variant="outline-danger">Delete</Button>
+                            </div>
+                        </td>
+                    </tr>
+            }
         </tbody>
-
     )
 }
